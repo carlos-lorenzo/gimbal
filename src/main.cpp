df4344d90ee2fa35c564cd7f3035846cbd7cb19b
Carlos lorenzo <clorenzozuniga@gmail.com>
2025-05-10 20:51:49 +0200

    - updated code, angle logic imporoved

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "utils.h"
 #include "Servo.h"
 
+void display_cumulative_angle();
+
 MPU6050 mpu;
 Servo z_servo;
 Servo y_servo;
@@ -13,11 +15,11 @@ int Calibrated[6] = {1321, 1105, 1527, 97, 21, 47};
 const int iAx = 0;
 const int iAy = 1;
 const int iAz = 2;
-const int iGx = 3;  
+const int iGx = 3;
 const int iGy = 4;
 const int iGz = 5;
 
-// #define OUTPUT_READABLE_ACCELGYRO
+//#define OUTPUT_READABLE_ACCELGYRO
 //#define OUTPUT_BINARY_ACCELGYRO
 #define OUTPUT_READABLE_YAWPITCHROLL
 //#define OUTPUT_READABLE_QUATERNION
@@ -28,7 +30,7 @@ const int iGz = 5;
 
 #define USE_DMP
 
-uint8_t MPUIntStatus; 
+uint8_t MPUIntStatus;
 uint16_t packetSize;
 uint8_t devStatus;
 uint8_t FIFOBuffer[64];
@@ -37,7 +39,7 @@ bool blinkState;
 
 
 
-/*---Orientation/Motion Variables---*/ 
+/*---Orientation/Motion Variables---*/
 Quaternion q;           // [w, x, y, z]         Quaternion container
 VectorInt16 aa;         // [x, y, z]            Accel sensor measurements
 VectorInt16 gy;         // [x, y, z]            Gyro sensor measurements
@@ -49,40 +51,53 @@ float ypr[3];           // [yaw, pitch, roll]   Yaw/Pitch/Roll container and gra
 
 uint16_t targetAngle = 90;
 
-bool motorsTriggered = false;
+bool motorsTriggered = true;
+
 float sensorTargetAngleY;
-float angleYToWrite;
-
 float sensorTargetAngleX;
-float angleXToWrite;
-
 float sensorTargetAngleZ;
-float angleZToWrite;
 
-int BUTTON_PIN = 2;
+float currentAngleX;
+float prevAngleX;
+float currentAngleY;
+float prevAngleY;
+
+float currentAngleZ;
+float prevAngleZ;
+float prevRawAngleZ = 0;
+
+
+float cumulativeAngleZ = targetAngle;
+float prevCumulativeAngleZ = targetAngle;
+float prevZWrittenAngle = targetAngle;
+float angleZToWrite = targetAngle;
+
+float cumulativeAngleX = targetAngle;
+float cumulativeAngleY = targetAngle;
+int BUTTON_PIN = 6;
 byte buttonState;
 
 int x_index = 0;
-int y_index = 1;
-int z_index = 2;
+int y_index = 1; //1
+int z_index = 1; // 2
 
 void setup() {
-	
+
 	Initialize(mpu);
 	SetOffsets(mpu, Calibrated);
 
-	
+
 
 	Serial.println(F("Initializing DMP..."));
   	devStatus = mpu.dmpInitialize();
-	
+
 	mpu.setXGyroOffset(Calibrated[iGx]);
 	mpu.setYGyroOffset(Calibrated[iGy]);
 	mpu.setZGyroOffset(Calibrated[iGz]);
 
 	Serial.println("These are the Active offsets: ");
 	mpu.PrintActiveOffsets();
-	
+
 	Serial.println(F("Enabling DMP..."));   //Turning ON DMP
 	mpu.setDMPEnabled(true);
 
@@ -97,71 +112,118 @@ void setup() {
   	Serial.println(F("Done :) "));
 
     z_servo.attach(9);
-	y_servo.attach(5);
-	x_servo.attach(3);
+	// y_servo.attach(5);
+	// x_servo.attach(3);
 	pinMode(BUTTON_PIN, INPUT_PULLUP);
-	
+
 
     z_servo.write(targetAngle);
-	y_servo.write(targetAngle);
-	x_servo.write(targetAngle);
+	// y_servo.write(targetAngle);
+	// x_servo.write(targetAngle);
 
 }
 
-void loop() {
-	
+void display_cumulative_angle() {
+
+	// Serial.print("X: ");
+	// Serial.print(cumulativeAngleX);
+	// Serial.print("\t");
+	// Serial.print("Y: ");
+	// Serial.print(cumulativeAngleY);
+	// Serial.print("\t");
+
+	Serial.print("Raw angle: ");
+	Serial.print(ypr[z_index] * 180/M_PI);
+	Serial.print("\t");
+
+	Serial.print("Prev angle: ");
+	Serial.print(prevAngleZ);
+	Serial.print("\t");
 
 	
+	Serial.print("Current angle: ");
+	Serial.print(currentAngleZ);
+	Serial.print("\t");
+
+	Serial.print("Difference: ");
+	Serial.print(currentAngleZ - prevAngleZ);
+	Serial.print("\t");
+
+	Serial.print("Cum Z: ");
+	Serial.println(cumulativeAngleZ);
+}
+
+
+void loop() {
+
+
+
 
 	#ifdef USE_DMP
-		if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) { // Get the Latest packet 
+		prevAngleX = currentAngleX;
+		prevAngleY = currentAngleY;
+		prevAngleZ = currentAngleZ;
+
+		prevCumulativeAngleZ = cumulativeAngleZ;
+
+		if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) { // Get the Latest packet
+			
 			mpu.dmpGetQuaternion(&q, FIFOBuffer);
 			mpu.dmpGetGravity(&gravity, &q);
 			mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
 			#ifdef OUTPUT_READABLE_YAWPITCHROLL
 				/* Display Euler angles in degrees */
-				Serial.print("ypr\t");
-				Serial.print(ypr[x_index] * 180/M_PI);
-				Serial.print("\t");
-				Serial.print(ypr[y_index] * 180/M_PI);
-				Serial.print("\t");
-				Serial.println(ypr[z_index] * 180/M_PI);
+				// Serial.print("ypr\t");
+				// Serial.print(ypr[x_index] * 180/M_PI);
+				// Serial.print("\t");
+				// Serial.print(ypr[y_index] * 180/M_PI);
+				// Serial.print("Raw angle: ");
+				// Serial.print(ypr[z_index] * 180/M_PI);
+				// Serial.print("\t");
 			#endif
+
+			currentAngleX = ypr[x_index] * 180/M_PI;
+			currentAngleY = ypr[y_index] * 180/M_PI;
+			currentAngleZ = ypr[z_index] * 180/M_PI;
 		}
+
+		if (currentAngleZ < -90 && prevRawAngleZ > 90) {
+			currentAngleZ = 360 - abs(currentAngleZ);
+		} else {
+			cumulativeAngleZ += currentAngleZ - prevAngleZ;
+		}
+		
+		display_cumulative_angle();
+		//cumulativeAngleX += currentAngleX - prevAngleX;
+		//cumulativeAngleY += currentAngleY - prevAngleY;
+		
+		prevRawAngleZ = ypr[z_index] * 180/M_PI;
+		
+
 		// Button logic
 		buttonState = digitalRead(BUTTON_PIN);
 		if (buttonState == LOW) {
-			motorsTriggered = !motorsTriggered;
-			if (motorsTriggered) {
-				sensorTargetAngleX = ypr[x_index] * 180/M_PI;
-				sensorTargetAngleY = ypr[y_index] * 180/M_PI;
-				sensorTargetAngleZ = ypr[z_index] * 180/M_PI;
-				Serial.println("ON: ");
-				Serial.println(sensorTargetAngleX);
-				Serial.println(sensorTargetAngleY);
-			} else {
-				Serial.println("OFF");
-			}
-			delay(200);
+			//motorsTriggered = !motorsTriggered;
 		}
-		
-	if (motorsTriggered) {
-		angleXToWrite = tapear(ypr[x_index] * 180/M_PI , sensorTargetAngleX - 90, sensorTargetAngleX + 90, 0, 180);
-		x_servo.write(angleXToWrite);
 
-		angleYToWrite = tapear(ypr[y_index] * 180/M_PI , sensorTargetAngleY - 90, sensorTargetAngleY + 90, 0, 180);
-		y_servo.write(angleYToWrite);
+		if (motorsTriggered) {
+			// angleXToWrite = tapear(ypr[x_index] * 180/M_PI , sensorTargetAngleX - 90, sensorTargetAngleX + 90, 0, 180);
+			// x_servo.write(angleXToWrite);
 
-		angleZToWrite = tapear(ypr[z_index] * 180/M_PI , sensorTargetAngleZ - 90, sensorTargetAngleZ + 90, 0, 180);
-		z_servo.write(angleZToWrite);
+			// angleYToWrite = tapear(ypr[y_index] * 180/M_PI , sensorTargetAngleY - 90, sensorTargetAngleY + 90, 0, 180);
+			// y_servo.write(angleYToWrite);
+			angleZToWrite = prevZWrittenAngle - cumulativeAngleZ + prevCumulativeAngleZ;
+			z_servo.write(angleZToWrite);
+			prevZWrittenAngle = angleZToWrite;
+			
+		} else {
+			z_servo.write(targetAngle);
+			y_servo.write(targetAngle);
+			x_servo.write(targetAngle);
+		}
 		delay(15);
-	} else {
-		z_servo.write(targetAngle);
-		y_servo.write(targetAngle);
-		x_servo.write(targetAngle);
-	}
 	#endif
-} 
+}
 
 
 
